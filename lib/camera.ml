c8276c71705706b7436d3c403cf57bac5c4db190
Nathan Ringo <nathan@remexre.xyz>
2021-12-17 17:11:18 -0600

    Adds cursor position events.

--- lib/camera.ml
+++ lib/camera.ml
@@ -9,40 +9,43 @@ type t = {
 
 let camera_direction pos =
   let camera_target = Vec3.zero in
-    Vec3.normalize Vec3.(pos - camera_target)
+  Vec3.normalize Vec3.(pos - camera_target)
 
 let camera_right dir =
   let up = (0.0, 1.0, 0.0) in
-    Vec3.cross up dir
+  Vec3.cross up dir
 
-let view (c : t) = 
+let view (c : t) =
   let dir = Vec3.(c.camera_pos + c.camera_front) in
-    let eye = c.camera_pos in
-      let up = c.camera_up in
-        Mat4.look_at ~eye ~dir ~up
+  let eye = c.camera_pos in
+  let up = c.camera_up in
+  Mat4.look_at ~eye ~dir ~up
 
 let init (pos : Vec3.t) =
   let cd = camera_direction pos in
   let cr = camera_right cd in
   let cu = Vec3.cross cd cr in
-  let cf = (0.0, 0.0, 0.0-.1.0) in
-    {camera_pos = pos;
+  let cf = (0.0, 0.0, 0.0 -. 1.0) in
+  {
+    camera_pos = pos;
     camera_speed = 0.5;
     camera_direction = cd;
     camera_right = cr;
     camera_up = cu;
     camera_front = cf;
-    }
+  }
 
 (* let camera_pos = (0.0, 0.0, 0.3) *)
 
 let process_input (c : t) (input : Window.event) (dt : float) =
-  let Key(key, _) = input in
-  let c_cfcu = Vec3.normalize (Vec3.cross c.camera_front c.camera_up) in
-  let speed = c.camera_speed *. dt in
-    match key with
-      GLFW.W -> c.camera_pos <- Vec3.(c.camera_pos + (c.camera_front * speed))
-    | GLFW.A -> c.camera_pos <- Vec3.(c.camera_pos - (c.camera_front * speed))
-    | GLFW.S -> c.camera_pos <- Vec3.(c.camera_pos - (c_cfcu * speed))
-    | GLFW.D -> c.camera_pos <- Vec3.(c.camera_pos + (c_cfcu * speed))
-    | _ -> ()
+  match input with
+  | CursorPos (_x, _y) -> failwith "TODO"
+  | Key (key, _) -> (
+      let c_cfcu = Vec3.normalize (Vec3.cross c.camera_front c.camera_up) in
+      let speed = c.camera_speed *. dt in
+      match key with
+      | GLFW.W -> c.camera_pos <- Vec3.(c.camera_pos + (c.camera_front * speed))
+      | GLFW.A -> c.camera_pos <- Vec3.(c.camera_pos - (c.camera_front * speed))
+      | GLFW.S -> c.camera_pos <- Vec3.(c.camera_pos - (c_cfcu * speed))
+      | GLFW.D -> c.camera_pos <- Vec3.(c.camera_pos + (c_cfcu * speed))
+      | _ -> ())
