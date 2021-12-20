5c1be633ca9977db89856994a10a39d4e7e1fa2b
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-12-19 18:52:04 -0600

    Fix error

--- src/main.ml
+++ src/main.ml
@@ -5,7 +5,7 @@ let main () : unit =
       Printf.printf "OpenGL driver from %s\n"
         (Option.get (Gl.get_string Gl.vendor));
       let camera = Camera.init (0.0, 0.5, 0.8) window in
-      let particle_system = Particle_system.init 0 (0.0, 0.0, -1.0) in
+      let particle_system = Particle_system.init (0.0, 0.0, -1.0) in
       let scene = Render.init_scene particle_system camera in
       Window.loop ~window (fun dt events ->
           (* TODO: Actually handle input events! For now, we just print how many there were. *)
